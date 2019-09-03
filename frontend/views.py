0db1090648588715b2e812ce019bf256a45e516a
Mathanraj-Sharma <rvmmathanraj@gmail.com>
2019-09-03 17:59:17 +0530

    login for frontend added

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
 def index(request):
     return render(request, "index2.html")
+
+def login(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+
+        user = auth.authenticate(username= username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else: 
+            messages.info(request, 'Invalid Credentials')
+            return redirect('login')
+    else:
+        return render(request, "login.html")
