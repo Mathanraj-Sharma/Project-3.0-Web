60de42988898ce6df0293f1094262bf5f23a202f
Mathanraj-Sharma <rvmmathanraj@gmail.com>
2019-09-05 22:42:52 +0530

    counts added to frontend

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from reviews.models import review
+from complaint.models import complaint
+from question.models import question
+from mobileusers.models import mobileuser
 
 # Create your views here.
 
@@ -12,10 +16,21 @@ def login(request):
         password = request.POST['pass']
 
         user = auth.authenticate(username= username, password = password)
+        review_count = review.objects.all().count()
+        complaint_count = complaint.objects.all().count()
+        question_count = question.objects.all().count()
+        mobileusers_count = mobileuser.objects.all().count()
 
+        context = {
+            'review_count': review_count,
+            'complaint_count': complaint_count,
+            'question_count': question_count,
+            'mobileusers_count': mobileusers_count
+        }
+        
         if user is not None:
             auth.login(request, user)
-            return redirect('index')
+            return render(request,'index2.html', context)
         else: 
             #messages.info(request, 'Invalid Credentials')
             return redirect('/')
