aa3516e80d74bfb69f63ff2fdb75e6a531fb6091
Mathanraj-Sharma <rvmmathanraj@gmail.com>
2019-10-10 14:40:50 +0530

    map added to on site view

--- hotel/views.py
+++ hotel/views.py
@@ -8,20 +8,33 @@ from complaint.models import complaint
 from question.models import question
 from mobileusers.models import mobileuser
 from graph.views import get_total_score_doughnut, get_question_pie
+import ast
 
 
 def hotel_list_view(request,id):
     hotel_detail = hotel.objects.all()
     hotelspecific = id
-    s, d = get_total_score_doughnut(id)
+    marker = get_marker(id)
+    overall_script, overall_div = get_total_score_doughnut(id)
     q_script, q_div = get_question_pie(id, "How good was the outside environment of the restaurant?")
     context = {
         'id': hotelspecific,
         'hotel_detail': hotel_detail,
-        's' : s,
-        'd' : d,
+        'marker' : marker,
+        'overall_script' : overall_script,
+        'overall_div' : overall_div,
         'q_script': q_script,
         'q_div': q_div
     }
     return render(request,'hotelview.html',context)
 
+def get_marker(hotel_id):
+    """
+        This function will return the list of markers for all hotels in review table
+    """
+    marker = []
+    latlng = ast.literal_eval(hotel.objects.filter(id = hotel_id).values('geo_tag')[0]['geo_tag'])
+    marker.append(latlng['longitude'])
+    marker.append(latlng['latitude'])
+        
+    return marker
