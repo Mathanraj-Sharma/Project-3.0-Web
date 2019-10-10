14e06eac6129134e4fcdd304d55ecccc7fd16fdb
Mathanraj-Sharma <rvmmathanraj@gmail.com>
2019-10-10 14:13:29 +0530

    sample graphs added to view on site

--- graph/views.py
+++ graph/views.py
@@ -55,16 +55,16 @@ def get_total_score_doughnut(hotel_id):
         'Rest' : (5-score)
     }
 
-    data = pd.Series(x).reset_index(name='value').rename(columns={'index':'country'})
+    data = pd.Series(x).reset_index(name='value').rename(columns={'index':'scoring'})
     data['angle'] = data['value']/5 * 2*pi
     data['color'] = ['blue', 'white']
 
-    p = figure(plot_height=350, title="Doughnut Chart", toolbar_location=None,
-            tools="hover", tooltips="@country: @value", x_range=(-0.5, 1.0))
+    p = figure(plot_height=250, plot_width = 375, title="Overall Score", toolbar_location=None,
+            tools="hover", tooltips="@scoring: @value", x_range=(-0.5, 1.0))
 
     p.annular_wedge(x=0, y=1,inner_radius = 0.2, outer_radius=0.4,
             start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
-            line_color="black", fill_color='color', legend='country', source=data)
+            line_color="black", fill_color='color', legend='scoring', source=data)
 
     p.axis.axis_label=None
     p.axis.visible=False
@@ -97,18 +97,18 @@ def get_question_pie(hotel_id, question):
     """
     
     x = dict(pd.DataFrame({'col1': get_question_answers(hotel_id,question)}).col1.value_counts())
-    print(x)
+    # print(x)
     
-    data = pd.Series(x).reset_index(name='value').rename(columns={'index':'country'})
+    data = pd.Series(x).reset_index(name='value').rename(columns={'index':'answer'})
     data['angle'] = data['value']/data['value'].sum() * 2*pi
     data['color'] = viridis(len(x))
 
-    p = figure(plot_height=350, title= question, toolbar_location=None,
-            tools="hover", tooltips="@country: @value", x_range=(-0.5, 1.0))
+    p = figure(plot_height=250, plot_width = 375, title= question, toolbar_location=None,
+            tools="hover", tooltips="@answer: @value", x_range=(-0.5, 1.0))
 
     p.wedge(x=0, y=1,radius=0.4,
             start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
-            line_color="black", fill_color='color', legend='country', source=data)
+            line_color="black", fill_color='color', legend='answer', source=data)
 
     p.axis.axis_label=None
     p.axis.visible=False
--- hotel/views.py
+++ hotel/views.py
@@ -7,13 +7,21 @@ from reviews.models import review
 from complaint.models import complaint
 from question.models import question
 from mobileusers.models import mobileuser
+from graph.views import get_total_score_doughnut, get_question_pie
 
 
 def hotel_list_view(request,id):
-        hotel_detail = hotel.objects.all()
-        hotelspecific = id
-        context = {
-            'id': hotelspecific,
-            'hotel_detail': hotel_detail
-        }
-        return render(request,'hotelview.html',context)
+    hotel_detail = hotel.objects.all()
+    hotelspecific = id
+    s, d = get_total_score_doughnut(id)
+    q_script, q_div = get_question_pie(id, "How good was the outside environment of the restaurant?")
+    context = {
+        'id': hotelspecific,
+        'hotel_detail': hotel_detail,
+        's' : s,
+        'd' : d,
+        'q_script': q_script,
+        'q_div': q_div
+    }
+    return render(request,'hotelview.html',context)
+
